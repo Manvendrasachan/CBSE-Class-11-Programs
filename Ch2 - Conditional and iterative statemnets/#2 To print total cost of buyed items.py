c34459ec810457535c3c9130069c3779fd12ec28
bi-nary-coder <72183731+bi-nary-coder@users.noreply.github.com>
2020-10-01 12:24:37 +0530

    # Simple Python Program

---
+++ Ch2 - Conditional and iterative statemnets/#2 To print total cost of buyed items.py
@@ -0,0 +1,8 @@
+'''
+A store charges ₹ 120 per item if you buy less than 10 items. If you buy between 10 and 99 items, the cost
+is ₹ 100 per item. If you buy 100 or more items, the cost is ₹ 70 per item. Write a program that asks
+user how many items they are buying and prints the total cost.
+'''
+#Solution-
+
+items=int(input("Enter the number of itemsy the customer is buying"))
