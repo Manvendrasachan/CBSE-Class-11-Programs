295697edea08ab685518cb7a1a0b9b266e131a16
the-indiancoder <72183731+the-indiancoder@users.noreply.github.com>
2020-10-01 11:01:23 +0530

    Program to check number divisibality

---
+++ Ch 1- Data Handling/#6 To check if the first number is fully divisible or not by second number.py
@@ -0,0 +1,17 @@
+#Question-Write a program to take two number and print if the first number is fully divisible by the second number or not.
+
+#Solution-
+
+first_number=int(input("Enter the first number"))
+second_number=int(input("Enter the second number"))
+
+if first_number%second_number==0:
+	print (f"{first_number} is fully divisible by {second_number}")
+else:
+	print (f"{first_number} is not fully divisible by {second_number}")
+
+
+'''
+ Follow me on GitHub( https://github.com/Manvendrasachan ) for Interesting Projects and cool programs
+ and all the programs of Class 11 and 12 CBSE board.
+ and F
