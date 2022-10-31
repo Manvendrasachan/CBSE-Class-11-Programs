d8f855bf6190aefb7329d90c0f391a55dcaae530
bi-nary-coder <72183731+bi-nary-coder@users.noreply.github.com>
2022-10-31 21:26:42 +0530

    Program to reverse a number

---
+++ Ch 1- Data Handling/#7 How to reverse a number.py
@@ -0,0 +1,8 @@
+#Question- Write a program to take 2-digit number and then print the reversed number.
+#Example- print 52 for input 25
+
+#Solution-
+
+number=input("Enter a number which you want to reverse")
+reversednumber=(number[::-1])
+print ("Reversed Number=",reversednumber) 
