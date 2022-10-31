3e24e8bba02602f6ef6b72a0c6e431d49245fd62
bi-nary-coder <72183731+bi-nary-coder@users.noreply.github.com>
2022-10-31 21:36:28 +0530

    Program to reverse a string

---
+++ Ch 1- Data Handling/#9 How to reverse a string.py
@@ -0,0 +1,8 @@
+#Question- Write a program to take string and then print the reversed string.
+#Example- print dsa for input asd
+
+#Solution-
+
+number=input("Enter a string which you want to reverse")
+reversedstring=(number[::-1])
+print ("Reversed String=",reversedstring) 
